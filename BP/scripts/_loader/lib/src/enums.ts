509b5c8547cee0861c197e73097447daa26555a5
Ruanzinho2 <ruanalmeida2883@gmail.com>
2026-01-08 09:12:57 -0300

    create BP/scripts/_loader/lib/src/enums.ts

---
+++ BP/scripts/_loader/lib/src/enums.ts
@@ -0,0 +1,26 @@
+export enum BossBarColor {
+    BLUE = 0,
+    GREEN = 1,
+    PINK = 2,
+    PURPLE = 3,
+    REBECCA_PURPLE = 4,
+    RED = 5,
+    WHITE = 6,
+    YELLOW = 7
+}
+
+export enum BossBarStyle {
+    SOLID = 0,
+    SEGMENTED_6 = 1,
+    SEGMENTED_10 = 2,
+    SEGMENTED_12 = 3,
+    SEGMENTED_20 = 4
+}
+
+export enum GroupRule {
+    NO_PVP = 0,
+    NO_DAMAGE = 1,
+    PVP_ONLY_GROUP = 3,
+    NO_PVP_NO_DAMAGE = 4,
+    NO_PVP_ONLY_GROUP = 5
+}
