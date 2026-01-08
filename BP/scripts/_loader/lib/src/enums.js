75bc0d15896e703b81ca9168155889eeb375a11d
Ruanzinho2 <ruanalmeida2883@gmail.com>
2026-01-08 09:12:49 -0300

    create BP/scripts/_loader/lib/src/enums.js

---
+++ BP/scripts/_loader/lib/src/enums.js
@@ -0,0 +1,27 @@
+export var BossBarColor;
+(function (BossBarColor) {
+    BossBarColor[BossBarColor["BLUE"] = 0] = "BLUE";
+    BossBarColor[BossBarColor["GREEN"] = 1] = "GREEN";
+    BossBarColor[BossBarColor["PINK"] = 2] = "PINK";
+    BossBarColor[BossBarColor["PURPLE"] = 3] = "PURPLE";
+    BossBarColor[BossBarColor["REBECCA_PURPLE"] = 4] = "REBECCA_PURPLE";
+    BossBarColor[BossBarColor["RED"] = 5] = "RED";
+    BossBarColor[BossBarColor["WHITE"] = 6] = "WHITE";
+    BossBarColor[BossBarColor["YELLOW"] = 7] = "YELLOW";
+})(BossBarColor || (BossBarColor = {}));
+export var BossBarStyle;
+(function (BossBarStyle) {
+    BossBarStyle[BossBarStyle["SOLID"] = 0] = "SOLID";
+    BossBarStyle[BossBarStyle["SEGMENTED_6"] = 1] = "SEGMENTED_6";
+    BossBarStyle[BossBarStyle["SEGMENTED_10"] = 2] = "SEGMENTED_10";
+    BossBarStyle[BossBarStyle["SEGMENTED_12"] = 3] = "SEGMENTED_12";
+    BossBarStyle[BossBarStyle["SEGMENTED_20"] = 4] = "SEGMENTED_20";
+})(BossBarStyle || (BossBarStyle = {}));
+export var GroupRule;
+(function (GroupRule) {
+    GroupRule[GroupRule["NO_PVP"] = 0] = "NO_PVP";
+    GroupRule[GroupRule["NO_DAMAGE"] = 1] = "NO_DAMAGE";
+    GroupRule[GroupRule["PVP_ONLY_GROUP"] = 3] = "PVP_ONLY_GROUP";
+    GroupRule[GroupRule["NO_PVP_NO_DAMAGE"] = 4] = "NO_PVP_NO_DAMAGE";
+    GroupRule[GroupRule["NO_PVP_ONLY_GROUP"] = 5] = "NO_PVP_ONLY_GROUP";
+})(GroupRule || (GroupRule = {}));
